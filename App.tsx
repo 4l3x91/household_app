2c526a108ac05cde253f76d1ade2625c55586187
osci-the-orca <88oscste@skola.boras.se>
2022-10-13 17:44:22 +0200

    wrapped in Provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
 import Navigation from "./navigation";
+import store from "./store/store";
 
 export default function App() {
   return (
-    <>
+    <ReduxProvider store={store}>
       <Navigation />
-    </>
+    </ReduxProvider>
   );
 }
