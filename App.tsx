4fa8c28372e8786b36bd0713bf0c3e268a336cd6
4l3x <alexander.auermaa@gmail.com>
2022-10-13 22:51:43 +0200

    paperprovider added, initial elements now using paper

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
 import Navigation from "./navigation";
+import { getTheme } from "./theme";
+import useColorScheme from "./hooks/useColorScheme";
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
-    <>
-      <Navigation />
-    </>
+    <PaperProvider theme={getTheme(colorScheme)}>
+      <Navigation colorScheme={colorScheme} />
+    </PaperProvider>
   );
 }
