8f0700fae263baee84405fc81ef54ee43f3d9b2d
4l3x-suvnet <alexander.auermaa@gmail.com>
2022-10-16 14:42:57 +0200

    mockdata used in ChoreDetailsScreen

--- screens/ChoreDetailsScreen.tsx
+++ screens/ChoreDetailsScreen.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import { selectChore } from "../store/chore/choreSelectors";
+import { useAppSelector } from "../store/store";
 
 const ChoreDetailsScreen = () => {
+  const chore = useAppSelector(selectChore);
   return (
     <View>
-      <Text>ChoreDetailsScreen</Text>
+      <Text>Chore Id: {chore.chore.id}</Text>
+      <Text>{chore.chore.name}</Text>
+      <Text>{chore.chore.description}</Text>
     </View>
   );
 };
