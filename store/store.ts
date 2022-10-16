ee2cfe40966c1731b4f964a2b3f8e4781a5de87a
osci-the-orca <88oscste@skola.boras.se>
2022-10-16 07:59:30 +0200

    added householdreducer

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { householdReducer } from "./household/householdSlice";
 
 const store = configureStore({
   reducer: {
-    // household: householdReducer
+    household: householdReducer,
   },
 });
 
