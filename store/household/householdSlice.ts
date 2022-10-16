b6ed0419968bea9667dec1fee5691d036a7aac12
osci-the-orca <88oscste@skola.boras.se>
2022-10-16 08:36:28 +0200

    createded ThunkAction and added to extraReducers..

--- store/household/householdSlice.ts
+++ store/household/householdSlice.ts
@@ -1,11 +1,37 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { HouseholdModel } from "./householdModel";
 import { HouseholdState, initialState } from "./householdState";
+
+const setHouseholdThunk = createAsyncThunk<HouseholdModel, HouseholdModel, { rejectValue: string }>(
+  "household/setHousehold",
+  async (household, thunkAPI) => {
+    // TODO: get household from firestore by loggedin profile
+    try {
+      return household;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
 
 const householdSlice = createSlice({
   name: "household",
   initialState,
   reducers: {
     setHousehold: (state, action: PayloadAction<HouseholdState>) => (state = action.payload),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(setHouseholdThunk.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(setHouseholdThunk.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.household = action.payload;
+    });
+    builder.addCase(setHouseholdThunk.rejected, (state) => {
+      state.isLoading = false;
+      state.error = "Error: no household data found";
+    });
   },
 });
 
