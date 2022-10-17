fd697d4c47c629d004fb3a2f9321fe1fa154c431
Zaai90 <erik.grune@gmail.com>
2022-10-17 20:58:57 +0200

    change selectors

--- store/chore/choreSelectors.ts
+++ store/chore/choreSelectors.ts
@@ -1,3 +1,4 @@
 import { AppState } from "../store";
 
-export const selectChore = (state: AppState) => state.chore;
+export const selectChores = (state: AppState) => state.chores;
+export const selectChore = (state: AppState, id: string) => selectChores(state).chores.find((chore) => chore.id === id);
