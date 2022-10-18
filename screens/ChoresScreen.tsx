e3497223787ba86a1ec55df6113c328753afcf4b
Zaai90 <erik.grune@gmail.com>
2022-10-18 17:52:20 +0200

    fixed theme styling and added functionality

--- screens/ChoresScreen.tsx
+++ screens/ChoresScreen.tsx
@@ -1,53 +1,72 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Pressable, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Surface, Text, useTheme } from "react-native-paper";
 import { ChoreStackParams } from "../navigation/ChoreStackNavigator";
 import { selectChores } from "../store/chore/choreSelectors";
+import { selectCompletedChores } from "../store/completedChore/completedChoreSelector";
 import { useAppSelector } from "../store/store";
 
 type Props = NativeStackScreenProps<ChoreStackParams>;
 
 const ChoresScreen = ({ navigation }: Props) => {
   const chores = useAppSelector(selectChores);
-  
+  const completedChores = useAppSelector(selectCompletedChores);
+  const { colors } = useTheme();
+
   return (
-    <View style={{ backgroundColor: "lightgrey", flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <Text>ChoresScreen</Text>
       {chores.chores.map((chore) => (
-        <Pressable
-          style={{
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-
-            flexDirection: "row",
-            borderRadius: 10,
-            justifyContent: "space-between",
-            margin: 5,
-            padding: 10,
-            backgroundColor: "white",
-          }}
-          key={chore.id}
-          onPress={() => navigation.navigate("ChoreDetailsScreen", { id: chore.id })}
-        >
-          <View>
-            <Text>{chore.name}</Text>
-          </View>
-          {chore.id === "one" ? (
+        <Pressable key={chore.id} onPress={() => navigation.navigate("ChoreDetailsScreen", { id: chore.id })}>
+          <Surface
+            style={{
+              flexDirection: "row",
+              borderRadius: 10,
+              justifyContent: "space-between",
+              margin: 5,
+              padding: 10,
+            }}
+          >
             <View>
-              <Text>🐷🐍🦊</Text>
+              <Text>{chore.name}</Text>
             </View>
-          ) : (
-            <View style={{ backgroundColor: "#F2F2F2", borderRadius: 50, height: 25, width: 25, alignItems: "center", justifyContent: "center" }}>
-              <Text>{chore.interval}</Text>
-            </View>
-          )}
+            {completedChores.completedChores.find((cc) => cc.choreId === chore.id) ? (
+              <View style={{ flexDirection: "row" }}>
+                {completedChores.completedChores.map(
+                  (cc) => cc.choreId === chore.id && <Text key={cc.profileId + "-" + cc.choreId + "-" + cc.date}>🐍</Text>
+                )}
+              </View>
+            ) : completedChores.completedChores.find(
+                (cc) => cc.choreId === chore.id && cc.date.setDate(cc.date.getDate() + chore.interval) > Date.now()
+              ) ? (
+              <Surface
+                style={{
+                  borderRadius: 50,
+                  height: 25,
+                  width: 25,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: colors.background,
+                }}
+              >
+                <Text>{chore.interval}</Text>
+              </Surface>
+            ) : (
+              <Surface
+                style={{
+                  borderRadius: 50,
+                  height: 25,
+                  width: 25,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: colors.notification,
+                }}
+              >
+                <Text>{chore.interval}</Text>
+              </Surface>
+            )}
+          </Surface>
         </Pressable>
       ))}
     </View>
