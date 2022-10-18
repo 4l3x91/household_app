b30a43ea70811b43b7ac38cc70bb7a243407d057
Carl Åfalk <carl.afalk@hotmail.se>
2022-10-19 00:17:29 +0200

    created a logInUser component

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,23 +1,12 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Formik } from "formik";
 import React, { useEffect } from "react";
-import { Button, Text, TextInput } from "react-native-paper";
-import styled from "styled-components/native";
-import * as Yup from "yup";
+import LoginUser from "../components/LogInUser";
 import { useAuthentication } from "../hooks/useAuthentication";
 import { RootStackParams } from "../navigation/RootStackNavigator";
-import { useAppDispatch } from "../store/store";
-import { signInUser } from "../store/user/userSlice";
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
-const validation = Yup.object().shape({
-  email: Yup.string().required("Email kan inte vara tomt"),
-  password: Yup.string().required("Lösenord kan inte vara tomt"),
-});
-
 const LoginScreen = ({ navigation }: Props) => {
-  const dispatch = useAppDispatch();
   const { user } = useAuthentication();
 
   useEffect(() => {
@@ -27,47 +16,9 @@ const LoginScreen = ({ navigation }: Props) => {
   }, [user]);
   return (
     <>
-      <Main>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={validation}
-          onSubmit={(values) => {
-            dispatch(
-              signInUser({
-                email: values.email,
-                password: values.password,
-              })
-            );
-          }}
-        >
-          {({ handleChange, handleSubmit, values, errors }) => {
-            return (
-              <>
-                <TextInput label="Email" mode={"outlined"} value={values.email} onChangeText={handleChange("email")} />
-                {errors.email && <Text>{errors.email}</Text>}
-                <TextInput
-                  label="Lösenord"
-                  mode={"outlined"}
-                  value={values.password}
-                  onChangeText={handleChange("password")}
-                  secureTextEntry={true}
-                />
-                {errors.password && <Text>{errors.password}</Text>}
-                <Button mode={"contained"} style={{ marginTop: 10 }} onPress={handleSubmit}>
-                  Logga in
-                </Button>
-              </>
-            );
-          }}
-        </Formik>
-      </Main>
+      <LoginUser />
     </>
   );
 };
 
 export default LoginScreen;
-
-const Main = styled.View`
-  flex: 1;
-  padding: 10px;
-`;
