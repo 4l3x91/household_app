9596c890a15982ce0c846141ed3fc9d20eb9dc0a
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-18 16:28:08 +0200

    slice now uses auth to create user

--- store/user/userSlice.ts
+++ store/user/userSlice.ts
@@ -1,35 +1,46 @@
-import { addDoc, collection } from "@firebase/firestore";
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { db } from "../../config/firebase";
-import { User } from "./userModel";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserCredential } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth/react-native";
+import { auth } from "../../config/firebase";
+import { User as StateUser } from "./userModel";
 import { initialState } from "./userState";
 
-export const setUser = createAsyncThunk<User, User, { rejectValue: string }>("user/setUser", async (user, thunkAPI) => {
-  try {
-    await addDoc(collection(db, "users"), { user });
-    return user;
-  } catch (error) {
-    return thunkAPI.rejectWithValue("Error setting user");
+export const createUser = createAsyncThunk<UserCredential, { email: string; password: string }, { rejectValue: string }>(
+  "user/createUser",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      return await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Error setting user");
+    }
   }
-});
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    login(state, action: PayloadAction<StateUser>) {
+      state.user = action.payload;
+    },
+    logout(state) {
+      state.user = null;
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(setUser.pending, (state) => {
+    builder.addCase(createUser.pending, (state) => {
       state.pending = true;
     });
-    builder.addCase(setUser.fulfilled, (state, action) => {
+    builder.addCase(createUser.fulfilled, (state, action) => {
       state.pending = false;
-      state.user = action.payload;
+      state.user = { id: action.payload.user.uid, email: action.payload.user.email };
     });
-    builder.addCase(setUser.rejected, (state, action) => {
+    builder.addCase(createUser.rejected, (state, action) => {
       state.pending = false;
       state.error = action.payload || "Unknown error setting user";
     });
   },
 });
 
+export const { login, logout } = userSlice.actions;
 export const userReducer = userSlice.reducer;
