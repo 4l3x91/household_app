6a85d779a542d390784da8e02b9c35d2389e8728
4l3x <alexander.auermaa@gmail.com>
2022-10-19 16:22:56 +0200

    temp navigation to selectThemeScreen added

--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -1,13 +1,14 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { signOut } from "firebase/auth";
 import React from "react";
-import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { TouchableOpacity, View } from "react-native";
+import { Button, Surface, Text } from "react-native-paper";
 import CreateHousehold from "../components/createHousehold";
 import { auth } from "../config/firebase";
-import { RootStackParams } from "../navigation/RootStackNavigator";
+import { MenuStackParams } from "../navigation/MenuStackNavigator";
 
-type Props = NativeStackScreenProps<RootStackParams>;
+type Props = NativeStackScreenProps<MenuStackParams>;
 
 const MenuScreen = ({ navigation }: Props) => {
   function handleSignOut() {
@@ -19,8 +20,19 @@ const MenuScreen = ({ navigation }: Props) => {
   }
 
   return (
-    <View>
-      <Text>MenuScreen</Text>
+    <View style={{ padding: 10 }}>
+    <Text variant="headlineSmall">Inställningar</Text>
+      <Text variant="bodyMedium">Anpassa din upplevelse i Hushållet.</Text>
+
+    <View style={{ marginVertical: 20 }}>
+      <TouchableOpacity onPress={() => navigation.navigate("SelectThemeScreen")}>
+        <Surface elevation={0} style={{ flexDirection: "row", alignItems: "center" }}>
+          <FontAwesome name="moon-o" size={22} color="white" style={{ marginRight: 10 }} />
+          <Text variant="bodyMedium">Mörkt läge</Text>
+        </Surface>
+      </TouchableOpacity>
+    </View>
+
       <CreateHousehold />
       <Button mode="contained" onPress={handleSignOut}>
         Logout
