fbf0db9467271657d60d6fb0088525fe630ec326
osci-the-orca <88oscste@skola.boras.se>
2022-10-19 08:33:10 +0200

    created selector for HouseholdId

--- store/household/householdSelector.ts
+++ store/household/householdSelector.ts
@@ -3,3 +3,4 @@ import { AppState } from "../store";
 export const selectHousehold = (state: AppState) => state.household;
 
 export const selectHouseholdName = (state: AppState) => state.household.household.name;
+export const selectHouseholdId = (state: AppState) => state.household.household.id;
