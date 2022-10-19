e8e7b94630eea414a90c248388d62f7cd881100a
4l3x <alexander.auermaa@gmail.com>
2022-10-19 16:08:50 +0200

    ThemeSelectorScreen added to menustack

--- navigation/MenuStackNavigator.tsx
+++ navigation/MenuStackNavigator.tsx
@@ -1,18 +1,21 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import MenuScreen from "../screens/MenuScreen";
+import SelectThemeScreen from "../screens/SelectThemeScreen";
 
 export type MenuStackParams = {
   MenuScreen: undefined;
+  SelectThemeScreen: undefined;
 };
 
-const UserStack = createNativeStackNavigator<MenuStackParams>();
+const MenuStack = createNativeStackNavigator<MenuStackParams>();
 
 const MenuStackNavigator = () => {
   return (
-    <UserStack.Navigator initialRouteName="MenuScreen">
-      <UserStack.Screen name="MenuScreen" component={MenuScreen} />
-    </UserStack.Navigator>
+    <MenuStack.Navigator initialRouteName="MenuScreen">
+      <MenuStack.Screen name="MenuScreen" component={MenuScreen} />
+      <MenuStack.Screen name="SelectThemeScreen" component={SelectThemeScreen} options={{title: "Mörkt läge"}} />
+    </MenuStack.Navigator>
   );
 };
 
