ab31562a8b35a8992db9d44cb9486777a75d54da
osci-the-orca <88oscste@skola.boras.se>
2022-10-19 08:31:28 +0200

    created array with avatars

---
+++ store/profile/profileData.ts
@@ -0,0 +1,36 @@
+import { Avatar } from "./profileModel";
+
+export const avatarData: Avatar[] = [
+  {
+    avatar: "🦊",
+    color: "#264653",
+  },
+  {
+    avatar: "🐷",
+    color: "#287271",
+  },
+  {
+    avatar: "🐸",
+    color: "#b18bda",
+  },
+  {
+    avatar: "🐥",
+    color: "#8ab17d",
+  },
+  {
+    avatar: "🐙",
+    color: "#e9c46a",
+  },
+  {
+    avatar: "🐬",
+    color: "#f4a261",
+  },
+  {
+    avatar: "🦉",
+    color: "#ffc600",
+  },
+  {
+    avatar: "🦄",
+    color: "#941c2f",
+  },
+];
