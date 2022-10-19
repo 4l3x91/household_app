7844a733b6fda589b23bf5421a35237ef6a94632
osci-the-orca <88oscste@skola.boras.se>
2022-10-19 08:32:48 +0200

    removed mock code from profileScreen

--- screens/UserProfileScreen.tsx
+++ screens/UserProfileScreen.tsx
@@ -1,7 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Image, Pressable, View } from "react-native";
-import { Text } from "react-native-paper";
 import { UserStackParams } from "../navigation/UserStackNavigator";
 import { selectProfile } from "../store/profile/profileSelectors";
 import { useAppSelector } from "../store/store";
@@ -10,19 +8,7 @@ type Props = NativeStackScreenProps<UserStackParams>;
 
 const UserProfileScreen = ({ navigation }: Props) => {
   const profile = useAppSelector(selectProfile);
-  return (
-    <View>
-      <Text style={{ alignSelf: "center", marginBottom: 30 }}>UserProfileScreen</Text>
-      <Text>Profile Id: {profile.profile.id}</Text>
-      <Text>User Id: {profile.profile.userId}</Text>
-      <Text>Household Id:{profile.profile.householdId}</Text>
-      <Text>Profile name: {profile.profile.profileName}</Text>
-      <Image source={{ uri: profile.profile.avatar }} style={{ width: 80, height: 80 }} />
-      <Pressable onPress={() => navigation.navigate("UserRewardScreen")}>
-        <Text>Go to User Reward Screen</Text>
-      </Pressable>
-    </View>
-  );
+  return <></>;
 };
 
 export default UserProfileScreen;
