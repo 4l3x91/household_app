c3f8cb125f1dcd751d5fa4e39b53440cd6c2eb59
osci-the-orca <88oscste@skola.boras.se>
2022-10-19 08:34:17 +0200

    added function in thunk to add profile to db

--- store/profile/profileSlice.ts
+++ store/profile/profileSlice.ts
@@ -1,16 +1,18 @@
+import { addDoc, collection } from "@firebase/firestore";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { db } from "../../config/firebase";
 import { Profile } from "./profileModel";
 import { initialState } from "./profileState";
 
-const setProfile = createAsyncThunk<Profile, Profile, { rejectValue: string }>("profile/setProfile", async (profile, thunkAPI) => {
-  // Api call to set profile
+export const setProfile = createAsyncThunk<Profile, Profile, { rejectValue: string }>("profile/setProfile", async (profile, thunkAPI) => {
   try {
+    await addDoc(collection(db, "profiles"), profile);
     return profile;
   } catch (error) {
+    console.log(error);
     return thunkAPI.rejectWithValue("Error setting profile");
   }
 });
-
 
 const profileSlice = createSlice({
   name: "profile",
@@ -19,17 +21,19 @@ const profileSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(setProfile.pending, (state) => {
       state.pending = true;
+      console.log("pending");
     });
     builder.addCase(setProfile.fulfilled, (state, action) => {
       state.pending = false;
-      state.profile = action.payload;
+      console.log("fullfilled");
+      state.profiles.push(action.payload);
     });
     builder.addCase(setProfile.rejected, (state, action) => {
       state.pending = false;
+      console.log("rejected");
       state.error = action.payload || "Unknown error";
     });
   },
 });
 
-// export const { deposit, withdrawal } = bankSlice.actions;
 export const profileReducer = profileSlice.reducer;
