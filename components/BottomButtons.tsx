b9271b557f7b70371cfeb6a275fa3c135a3de477
Carl Åfalk <carl.afalk@hotmail.se>
2022-10-20 22:28:32 +0200

    created bottom buttons component

---
+++ components/BottomButtons.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View } from "react-native";
+import { Button, Divider } from "react-native-paper";
+
+interface Props {
+  handleSubmit: (e?: React.FormEvent<HTMLFormElement> | undefined) => void;
+  pending: boolean;
+  close: () => void;
+  leftTitle: string;
+  rightTitle: string;
+  leftIcon: string;
+  rightIcon: string;
+}
+
+const BottomButtons = ({ handleSubmit, pending, close, leftTitle, rightTitle, leftIcon, rightIcon }: Props) => {
+  return (
+    <View style={{ flexDirection: "row", marginBottom: 30 }}>
+      <View style={{ flex: 1 }}>
+        <Button labelStyle={{ fontSize: 16 }} mode={"text"} style={{ borderRadius: 0 }} onPress={handleSubmit} loading={pending} icon={leftIcon}>
+          {leftTitle}
+        </Button>
+      </View>
+      <Divider style={{ width: 1, height: "100%" }} />
+      <View style={{ flex: 1 }}>
+        <Button labelStyle={{ fontSize: 16 }} mode={"text"} style={{ borderRadius: 0 }} onPress={close} icon={rightIcon}>
+          {rightTitle}
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default BottomButtons;
