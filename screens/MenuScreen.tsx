19b5affd46135965b9f8767983527f2e2aee4a1e
4l3x <alexander.auermaa@gmail.com>
2022-10-20 23:52:42 +0200

    styling

--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -3,7 +3,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
-import { Button, Surface, Text } from "react-native-paper";
+import { Button, Surface, Text, useTheme } from "react-native-paper";
 import CreateHousehold from "../components/createHousehold";
 import { auth } from "../config/firebase";
 import { MenuStackParams } from "../navigation/MenuStackNavigator";
@@ -11,6 +11,7 @@ import { MenuStackParams } from "../navigation/MenuStackNavigator";
 type Props = NativeStackScreenProps<MenuStackParams>;
 
 const MenuScreen = ({ navigation }: Props) => {
+  const { colors } = useTheme();
   function handleSignOut() {
     signOut(auth).then(() => {
       {
@@ -21,17 +22,17 @@ const MenuScreen = ({ navigation }: Props) => {
 
   return (
     <View style={{ padding: 10 }}>
-    <Text variant="headlineSmall">Inställningar</Text>
+      <Text variant="headlineSmall">Inställningar</Text>
       <Text variant="bodyMedium">Anpassa din upplevelse i Hushållet.</Text>
 
-    <View style={{ marginVertical: 20 }}>
-      <TouchableOpacity onPress={() => navigation.navigate("SelectThemeScreen")}>
-        <Surface elevation={0} style={{ flexDirection: "row", alignItems: "center" }}>
-          <FontAwesome name="moon-o" size={22} color="white" style={{ marginRight: 10 }} />
-          <Text variant="bodyMedium">Mörkt läge</Text>
-        </Surface>
-      </TouchableOpacity>
-    </View>
+      <View style={{ marginVertical: 20 }}>
+        <TouchableOpacity onPress={() => navigation.navigate("SelectThemeScreen")}>
+          <Surface elevation={0} style={{ flexDirection: "row", alignItems: "center" }}>
+            <FontAwesome name="moon-o" size={22} color={colors.primary} style={{ marginRight: 10 }} />
+            <Text variant="bodyMedium">Mörkt läge</Text>
+          </Surface>
+        </TouchableOpacity>
+      </View>
 
       <CreateHousehold />
       <Button mode="contained" onPress={handleSignOut}>
