f233463d81f7f48b1d829c60bf526da2b42d49c3
4l3x <alexander.auermaa@gmail.com>
2022-10-20 23:46:14 +0200

    if statements added for colorScheme

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,5 +1,8 @@
 import { ColorSchemeName, useColorScheme as _useColorScheme } from 'react-native';
 
-export default function useColorScheme(): NonNullable<ColorSchemeName> {
-  return _useColorScheme() as NonNullable<ColorSchemeName>;
+export default function useColorScheme(theme?: ColorSchemeName): NonNullable<ColorSchemeName> {
+    const colorScheme = _useColorScheme() as NonNullable<ColorSchemeName>;
+    if (theme === 'light') return "light";
+    else if (theme === 'dark') return "dark";
+    else return colorScheme;
 }
