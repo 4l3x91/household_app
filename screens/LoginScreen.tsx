67bc5a550b44f3eb41c85ad1cccbf26d4a89c441
Carl Åfalk <carl.afalk@hotmail.se>
2022-10-20 16:55:07 +0200

    fixed navigaton depending on households

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,19 +1,36 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 import LoginUser from "../components/user/LogInUser";
-import { useAuthentication } from "../hooks/useAuthentication";
 import { RootStackParams } from "../navigation/RootStackNavigator";
+import { selectUsersProfiles } from "../store/profile/profileSelectors";
+import { findUsersProfilesThunk } from "../store/profile/profileSlice";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { selectUser } from "../store/user/userSelectors";
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
 const LoginScreen = ({ navigation }: Props) => {
-  const { user } = useAuthentication();
+  const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
+  const userProfiles = useAppSelector(selectUsersProfiles);
 
   useEffect(() => {
     if (user) {
-      navigation.navigate("HouseholdOptions");
+      dispatch(findUsersProfilesThunk(user));
+      if (userProfiles.length === 1) {
+        navigation.navigate("TabStack");
+      } else if (userProfiles.length > 1) {
+        navigation.navigate("HouseholdOptions");
+      } else navigation.navigate("HouseholdOptions");
+      console.log(userProfiles.length);
     }
   }, [user]);
+
+  // useEffect(() => {
+  //   if (user) {
+  //     dispatch(findUsersProfilesThunk(user));
+  //   }
+  // }, []);
   return (
     <>
       <LoginUser />
