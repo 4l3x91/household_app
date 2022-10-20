225d804a9a723b93faeb943703addf0a02372589
osci-the-orca <88oscste@skola.boras.se>
2022-10-20 09:20:18 +0200

    selector to get users Profiles

--- store/profile/profileSelectors.ts
+++ store/profile/profileSelectors.ts
@@ -1,3 +1,5 @@
 import { AppState } from "../store";
 
 export const selectProfile = (state: AppState) => state.profile;
+
+export const selectUsersProfiles = (state: AppState) => state.profile.profiles;
