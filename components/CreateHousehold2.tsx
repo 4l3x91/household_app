59150ff59d8aa5c0c695acaac5c280dc46fe32f2
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-21 11:23:07 +0200

    added tooltip, some cleanup, extracted yup schemas

--- components/CreateHousehold2.tsx
+++ components/CreateHousehold2.tsx
@@ -1,10 +1,10 @@
+import { AntDesign } from "@expo/vector-icons";
 import { Formik } from "formik";
 import React, { useState } from "react";
-import {} from "react-native";
-import { Button, Surface, Text, useTheme } from "react-native-paper";
+import { Button, IconButton, Surface, Text, useTheme } from "react-native-paper";
+import Tooltip from "rn-tooltip";
 import styled from "styled-components/native";
 import { v4 as uuidv4 } from "uuid";
-import * as Yup from "yup";
 import { HouseholdModel } from "../store/household/householdModel";
 import { createHouseholdThunk } from "../store/household/householdSlice";
 import { avatarData } from "../store/profile/profileData";
@@ -12,18 +12,9 @@ import { Avatar, Profile } from "../store/profile/profileModel";
 import { createProfile } from "../store/profile/profileSlice";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { selectUser } from "../store/user/userSelectors";
+import { generateHouseholdCode } from "../utils/utils";
+import { createHouseholdSchema } from "../utils/yupSchemas";
 import Input from "./Input";
-
-const householdNameSchema = Yup.object().shape({
-  householdName: Yup.string()
-    .required("namnet på hushållet kan inte vara tomt")
-    .matches(/^\S+(?: \S+)*$/, "ogiltligt hushålls namn")
-    .min(2, "namnet måste innehålla minst 2 tecken"),
-  profileName: Yup.string()
-    .required("Profilnamn får inte vara tomt")
-    .min(2, "Profilnamn måste minst inehålla 2 tecken")
-    .matches(/^\S*$/, "Profilnamn kan inte innehålla mellanslag"),
-});
 
 interface Props {
   closeModal: () => void;
@@ -31,49 +22,48 @@ interface Props {
 
 const CreateHousehold2 = ({ closeModal }: Props) => {
   const [avatar, setAvatar] = useState<Avatar>({} as Avatar);
+  const [householdCode, setHouseholdCode] = useState(generateHouseholdCode());
   const [selectedAvatar, setSelectedAvatar] = useState(-1);
-  const user = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
   const { colors } = useTheme();
+  const dispatch = useAppDispatch();
   const householdPending = useAppSelector((state) => state.household).pending;
   const profilePending = useAppSelector((state) => state.profile).pending;
-
-  const generateHouseholdCode = () => {
-    return Math.random().toString(36).slice(2, 8).toUpperCase();
-  };
+  const user = useAppSelector(selectUser);
 
   const pending = householdPending || profilePending;
+
+  const handleSubmit = (values: { householdName: string; profileName: string }) => {
+    const newHousehold: HouseholdModel = {
+      name: values.householdName,
+      id: uuidv4(),
+      code: householdCode,
+    };
+    dispatch(createHouseholdThunk(newHousehold));
+
+    if (user) {
+      const newProfile: Profile = {
+        id: uuidv4(),
+        userId: user?.id,
+        householdId: newHousehold.id,
+        profileName: values.profileName,
+        avatar: avatar,
+        role: "owner",
+        isPaused: false,
+      };
+      dispatch(createProfile(newProfile));
+    }
+    closeModal();
+  };
 
   return (
     <Container>
       <Formik
-        validationSchema={householdNameSchema}
+        validationSchema={createHouseholdSchema}
         initialValues={{
           householdName: "",
           profileName: "",
         }}
-        onSubmit={(values) => {
-          const newHousehold: HouseholdModel = {
-            name: values.householdName,
-            id: uuidv4(),
-            code: generateHouseholdCode(),
-          };
-          dispatch(createHouseholdThunk(newHousehold));
-
-          if (user) {
-            const newProfile: Profile = {
-              id: uuidv4(),
-              userId: user?.id,
-              householdId: newHousehold.id,
-              profileName: values.profileName,
-              avatar: avatar,
-              role: "owner",
-              isPaused: false,
-            };
-            dispatch(createProfile(newProfile));
-            closeModal();
-          }
-        }}
+        onSubmit={(values) => handleSubmit(values)}
       >
         {({ handleChange, handleSubmit, values, errors }) => {
           const inputsOk =
@@ -95,6 +85,24 @@ const CreateHousehold2 = ({ closeModal }: Props) => {
                   activeOutlineColor={colors.primary}
                 />
                 {errors.householdName && <Text>{errors.householdName}</Text>}
+                <CodeContainer elevation={3}>
+                  <Tooltip
+                    backgroundColor={colors.surfaceVariant}
+                    width={200}
+                    height={80}
+                    popover={<Text>Hushållskoden används för att bjuda in nya medlemmar till ditt hushåll.</Text>}
+                    actionType="press"
+                  >
+                    <CodeInnerContainer>
+                      <AntDesign name="questioncircleo" size={15} color={colors.onSurface} />
+                      <Text variant="bodyLarge"> Hushållskod: </Text>
+                    </CodeInnerContainer>
+                  </Tooltip>
+                  <CodeInnerContainer>
+                    <Text variant="bodyLarge">{householdCode}</Text>
+                    <IconButton icon="cached" size={15} onPress={() => setHouseholdCode(generateHouseholdCode())} mode="contained-tonal" />
+                  </CodeInnerContainer>
+                </CodeContainer>
                 <Input label="Profilnamn" value={values.profileName} handleChange={handleChange("profileName")} />
                 {errors.profileName && <Text>{errors.profileName}</Text>}
               </InputContainer>
@@ -118,15 +126,9 @@ const CreateHousehold2 = ({ closeModal }: Props) => {
                   ))}
                 </AvatarContent>
               </AvatarContainer>
-              <Button disabled={!inputsOk} mode="outlined" onPress={() => handleSubmit()} loading={pending}>
-                <Text>Skapa</Text>
+              <Button disabled={!inputsOk} mode="contained" onPress={() => handleSubmit()} loading={pending} buttonColor={colors.surfaceVariant}>
+                <Text style={{ color: colors.onSurfaceVariant }}>Skapa</Text>
               </Button>
-              {/* <CreateButton disabled onPress={() => handleSubmit()}>
-                <CreateButtonText color={colors.secondary}>Skapa</CreateButtonText>
-              </CreateButton> */}
-              {/* <CreateButton onPress={() => handleSubmit()} bgColor={colors.primary}>
-                <ButtonText color={colors.secondary}>Skapa hushåll</ButtonText>
-              </CreateButton> */}
             </Container>
           );
         }}
@@ -146,26 +148,6 @@ const InputContainer = styled.View`
   padding: 10px;
 `;
 
-const CreateButton = styled.Pressable`
-  margin: 20px 50px;
-  border-radius: 100px;
-
-  justify-content: center;
-  align-items: center;
-`;
-
-const CreateButtonText = styled.Text<{ color: string }>`
-  color: ${(props) => props.color};
-  font-size: 20px;
-  padding: 10px;
-`;
-
-const ButtonText = styled.Text<{ color: string }>`
-  color: ${(props) => props.color};
-  font-size: 20px;
-  padding: 10px;
-`;
-
 const AvatarCard = styled.Pressable<{ color: string; selected?: boolean }>`
   padding: 5px;
   background-color: ${(props) => props.color};
@@ -182,15 +164,28 @@ const AvatarContainer = styled.View`
 
 const AvatarContent = styled(Surface)`
   margin: 20px;
-  width: 85%;
+  width: 100%;
   align-items: center;
   flex-direction: row;
+  justify-content: center;
   flex-wrap: wrap;
-  /* background-color: lightgray; */
   border-radius: 10px;
   padding: 10px;
 `;
 
 const AvatarText = styled.Text`
   font-size: 40px;
+`;
+
+const CodeContainer = styled(Surface)`
+  padding: 0 5px;
+  margin: 10px 0;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const CodeInnerContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
