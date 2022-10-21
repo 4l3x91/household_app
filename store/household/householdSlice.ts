ab716def05c34a99ed85705389604568a639c688
osci-the-orca <88oscste@skola.boras.se>
2022-10-21 13:05:56 +0200

    created Thunk to delete household, state becomes {}, also need to delete all profiles

--- store/household/householdSlice.ts
+++ store/household/householdSlice.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs, query, where } from "@firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, query, where } from "@firebase/firestore";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { db } from "../../config/firebase";
 import { HouseholdModel } from "./householdModel";
@@ -8,34 +8,58 @@ export const createHouseholdThunk = createAsyncThunk<HouseholdModel, HouseholdMo
   "household/setHousehold",
   async (household, thunkAPI) => {
     try {
-      await addDoc(collection(db, "households"), household);
+      const collectionRef = collection(db, "households");
+
+      await addDoc(collectionRef, household);
+
       return household;
     } catch (error) {
       return thunkAPI.rejectWithValue("something went wrong");
     }
   }
-  );
+);
 
-  export const getHouseholdByCodeThunk = createAsyncThunk<HouseholdModel, string, { rejectValue: string }>(
-    "household/getHouseholdByCode",
-    async (code, thunkAPI) => {
-      try {
-        const householdRef = collection(db, "households");
-        const q = query(householdRef, where("code", "==", code));
-        const queryResult = await getDocs(q);
-        if (!queryResult.empty) {
-          const household = queryResult.docs[0].data() as HouseholdModel
-          return household;
-        }
-        else {
-          return thunkAPI.rejectWithValue("Household does not exist");
-        }
+export const deleteHouseholdThunk = createAsyncThunk<HouseholdModel, HouseholdModel, { rejectValue: string }>(
+  "household/deleteHousehold",
+  async (household, thunkAPI) => {
+    //TODO: think about logic to delete all profiles corresponding to this household. USE this with caution :]
+    try {
+      const collectionRef = collection(db, "households");
 
-      } catch (error) {
-        return thunkAPI.rejectWithValue("something went wrong");
+      const q = query(collectionRef, where("id", "==", household.id));
+
+      const result = await getDocs(q);
+
+      if (!result.empty) {
+        const householdToDelete = result.docs[0].id;
+        await deleteDoc(doc(db, "households", householdToDelete));
+      }
+      return {} as HouseholdModel;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue("error");
     }
   }
-)
+);
+
+export const getHouseholdByCodeThunk = createAsyncThunk<HouseholdModel, string, { rejectValue: string }>(
+  "household/getHouseholdByCode",
+  async (code, thunkAPI) => {
+    try {
+      const householdRef = collection(db, "households");
+      const q = query(householdRef, where("code", "==", code));
+      const queryResult = await getDocs(q);
+      if (!queryResult.empty) {
+        const household = queryResult.docs[0].data() as HouseholdModel;
+        return household;
+      } else {
+        return thunkAPI.rejectWithValue("Household does not exist");
+      }
+    } catch (error) {
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
 
 const householdSlice = createSlice({
   name: "household",
@@ -62,6 +86,18 @@ const householdSlice = createSlice({
       state.household = action.payload;
     });
     builder.addCase(getHouseholdByCodeThunk.rejected, (state) => {
+      state.isLoading = false;
+      state.error = "Error: no household data found";
+    });
+    builder.addCase(deleteHouseholdThunk.pending, (state) => {
+      state.isLoading = true;
+      state.error = "";
+    });
+    builder.addCase(deleteHouseholdThunk.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.household = action.payload;
+    });
+    builder.addCase(deleteHouseholdThunk.rejected, (state) => {
       state.isLoading = false;
       state.error = "Error: no household data found";
     });
