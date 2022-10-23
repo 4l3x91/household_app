9544ac3f70559ecc266454789879b2a0945f5248
Zaai90 <erik.grune@gmail.com>
2022-10-23 17:04:02 +0200

    add refresh chores and init fetch of chores

--- screens/ChoresScreen.tsx
+++ screens/ChoresScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useRef } from "react";
-import { Pressable, ScrollView, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Pressable, RefreshControl, ScrollView, View } from "react-native";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import { Modalize } from "react-native-modalize";
 import { Button, Divider, Portal, Text, useTheme } from "react-native-paper";
@@ -8,26 +8,50 @@ import CreateChore from "../components/chore/CreateChore";
 import ChoreItem from "../components/ChoreItem";
 import { ChoreStackParams } from "../navigation/ChoreStackNavigator";
 import { selectChores } from "../store/chore/choreSelectors";
-import { useAppSelector } from "../store/store";
+import { setChoresThunk } from "../store/chore/choreSlice";
+import { useAppDispatch, useAppSelector } from "../store/store";
 
 type Props = NativeStackScreenProps<ChoreStackParams>;
 
 const ChoresScreen = ({ navigation }: Props) => {
+  const household = useAppSelector((state) => state.household.household);
+  const dispatch = useAppDispatch();
   const chores = useAppSelector(selectChores);
   const modalizeRef = useRef<Modalize>(null);
   const theme = useTheme();
   const openModalize = () => {
     modalizeRef.current?.open();
   };
+  const [refresh, setRefresh] = React.useState(false);
+
+  useEffect(() => {
+    if (household) {
+      console.log("spam");
+      dispatch(setChoresThunk(household.id));
+    }
+  }, [household]);
+
   return (
     <View style={{ flex: 1 }}>
       <Text>ChoresScreen</Text>
-      <ScrollView>
-      {chores.chores.map((chore) => (
-        <Pressable key={chore.id} onPress={() => navigation.navigate("ChoreDetailsScreen", { id: chore.id, name: chore.name })}>
-          <ChoreItem chore={chore} />
-        </Pressable>
-      ))}
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refresh}
+            onRefresh={() => {
+              setRefresh(true);
+              dispatch(setChoresThunk(household.id));
+              console.log("refreshed");
+              setRefresh(false);
+            }}
+          />
+        }
+      >
+        {chores.chores.map((chore) => (
+          <Pressable key={chore.id} onPress={() => navigation.navigate("ChoreDetailsScreen", { id: chore.id, name: chore.name })}>
+            <ChoreItem chore={chore} />
+          </Pressable>
+        ))}
       </ScrollView>
       <Portal>
         <Modalize ref={modalizeRef} modalStyle={{ backgroundColor: theme.colors.surface }} adjustToContentHeight={true}>
