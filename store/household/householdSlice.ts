78fea599cfa5c264fc4cf6279ddeb1587e2ec08a
Carl Åfalk <carl.afalk@hotmail.se>
2022-10-24 14:50:19 +0200

    thunk to check if user has profile in household

--- store/household/householdSlice.ts
+++ store/household/householdSlice.ts
@@ -1,6 +1,8 @@
 import { addDoc, collection, getDocs, query, where } from "@firebase/firestore";
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 import { db } from "../../config/firebase";
+import { User } from "../user/userModel";
 import { HouseholdModel } from "./householdModel";
 import { initialState } from "./householdState";
 
@@ -16,21 +18,36 @@ export const createHouseholdThunk = createAsyncThunk<HouseholdModel, HouseholdMo
   }
 );
 
-export const getHouseholdByCodeThunk = createAsyncThunk<HouseholdModel, string, { rejectValue: string }>(
+export const getHouseholdByCodeThunk = createAsyncThunk<HouseholdModel, { code: string; user: User }, { rejectValue: string }>(
   "household/getHouseholdByCode",
-  async (code, thunkAPI) => {
+  async ({ code, user }, thunkAPI) => {
     try {
+      const profilesRef = collection(db, "profiles");
       const householdRef = collection(db, "households");
       const q = query(householdRef, where("code", "==", code));
+      const q2 = query(profilesRef, where("userId", "==", user.id));
       const queryResult = await getDocs(q);
+
       if (!queryResult.empty) {
         const household = queryResult.docs[0].data() as HouseholdModel;
+
+        console.log(household.name);
+
+        const queryResult2 = await getDocs(q2);
+        queryResult2.forEach((doc) => {
+          if (doc.get("householdId") === household.id) {
+            throw new Error("Du har redan en profil i detta hushåll");
+          }
+        });
         return household;
       } else {
-        return thunkAPI.rejectWithValue("Household does not exist");
+        return thunkAPI.rejectWithValue("Det här hushållet existerar inte");
       }
     } catch (error) {
-      return thunkAPI.rejectWithValue("something went wrong");
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      return thunkAPI.rejectWithValue("error");
     }
   }
 );
@@ -38,7 +55,11 @@ export const getHouseholdByCodeThunk = createAsyncThunk<HouseholdModel, string, 
 const householdSlice = createSlice({
   name: "household",
   initialState,
-  reducers: {},
+  reducers: {
+    setError(state, action: PayloadAction<string>) {
+      state.error = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(createHouseholdThunk.pending, (state) => {
       state.pending = true;
@@ -59,11 +80,12 @@ const householdSlice = createSlice({
       state.pending = false;
       state.household = action.payload;
     });
-    builder.addCase(getHouseholdByCodeThunk.rejected, (state) => {
+    builder.addCase(getHouseholdByCodeThunk.rejected, (state, action) => {
       state.pending = false;
-      state.error = "Error: no household data found";
+      state.error = action.payload || "unknown error";
     });
   },
 });
 
 export const householdReducer = householdSlice.reducer;
+export const { setError } = householdSlice.actions;
