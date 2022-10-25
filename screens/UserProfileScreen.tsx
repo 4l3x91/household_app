cccce880dd2a3de450c84d72b8db20b78925e886
Alexander Auermaa <alexander.auermaa@gmail.com>
2022-10-25 12:11:41 +0200

    modal design done'ish

--- screens/UserProfileScreen.tsx
+++ screens/UserProfileScreen.tsx
@@ -1,13 +1,18 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
-import { ScrollView, View } from "react-native";
-import { Button, Surface, Text, useTheme } from "react-native-paper";
+import { Alert, Modal, Pressable, ScrollView, View } from "react-native";
+import { Button, Surface, Text, TextInput, useTheme } from "react-native-paper";
 import { UserStackParams } from "../navigation/UserStackNavigator";
 import { selectHouseholdName } from "../store/household/householdSelector";
 import { Avatar } from "../store/profile/profileModel";
 import { selectCurrentProfile } from "../store/profile/profileSelectors";
 import { useAppSelector } from "../store/store";
+import { SimpleLineIcons } from '@expo/vector-icons'; 
 import { selectUser } from "../store/user/userSelectors";
+import CreateHousehold2 from "../components/CreateHousehold2";
+import { avatarData } from "../store/profile/profileData";
+import Input from "../components/Input";
+
 
 type Props = NativeStackScreenProps<UserStackParams, "UserProfileScreen">;
 
@@ -15,9 +20,11 @@ const UserProfileScreen = ({ navigation, route }: Props) => {
   const { colors } = useTheme();
   const [avatar, setAvatar] = useState<Avatar>({} as Avatar);
   const [selectedAvatar, setSelectedAvatar] = useState(-1);
+  const [overlay, setOverlay] = useState(false);
   const user = useAppSelector(selectUser);
   const profile = useAppSelector(selectCurrentProfile);
   const householdName = useAppSelector(selectHouseholdName);
+  const [modalVisible, setModalVisible] = useState(false);
   // TODO fix navigation typings
   return (
     <View>
@@ -37,7 +44,14 @@ const UserProfileScreen = ({ navigation, route }: Props) => {
                 <Text variant="headlineMedium">{profile?.profileName}</Text>
               </View>
             </Surface>
-            <Button onPress={() => console.log("Go to edit profile")}>Redigera profil</Button>
+            <Button onPress={() => {
+              setModalVisible(true)
+              setTimeout(() => {
+                setOverlay(true)
+                
+              }, 500)
+            }
+              }>Redigera profil</Button>
           </View>
           {/* <List.Accordion
               title="Mina hushåll"
@@ -54,11 +68,54 @@ const UserProfileScreen = ({ navigation, route }: Props) => {
             theme={{ colors: { background: "transparent" } }}
             style={{ backgroundColor: colors.surface, borderRadius: 10 }}
           >
-            <CreateHousehold2 />
-          </List.Accordion> */}
+        </List.Accordion> */}
           
         </ScrollView>
       </Surface>
+      <View style={{    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"}}>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        
+        visible={modalVisible}
+        >
+              <View style={{    flex: 1,
+    justifyContent: "center",
+      backgroundColor: overlay ? 'rgba(0,0,0,0.5)' : undefined,
+    alignItems: "center"}}>
+          <Surface style={{    margin: 20,
+          paddingVertical: 20,
+          borderRadius: 20,
+          alignItems: "center",
+          shadowColor: "#000"}}>
+            <Text>Namn: {profile?.profileName}</Text>
+            <Text>Välj en ledig avatar:</Text>
+            <View style={{justifyContent: 'center', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap',  paddingHorizontal: 40, paddingVertical: 10}}>
+              {avatarData.map((x) => (
+                <Text key={x.avatar} variant="headlineLarge" style={{
+                  backgroundColor: x.color, padding: 10, margin: 5, borderRadius: 10,
+                  overflow: 'hidden',
+                  opacity: x.avatar === profile?.avatar.avatar ? 1 : 0.5,
+                }}>{x.avatar}</Text>
+              ))}
+            </View>
+          </Surface>
+          <Pressable onPress={() => 
+            {
+              setOverlay(false);
+              setTimeout(() => {
+                setModalVisible(false);
+              }, 25)
+            }
+            }>
+          <SimpleLineIcons name="close" size={42} color={colors.primary} />
+          </Pressable>
+        </View>
+      </Modal>
+      </View>
     </View>
   );
 };
