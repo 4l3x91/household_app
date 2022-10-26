74c4d4a163e9a07402b8f10b848afdcd5b6f8755
Zaai90 <erik.grune@gmail.com>
2022-10-27 00:51:39 +0200

    add selectAllHouseholdMembers

--- store/profile/profileSelectors.ts
+++ store/profile/profileSelectors.ts
@@ -12,6 +12,9 @@ export const selectUserProfiles = (state: AppState) => {
 export const selectHouseholdMembers = (state: AppState) => {
   return selectProfile(state).profiles.filter((x) => x.userId !== state.userState.user?.id && x.householdId === state.household.household.id);
 };
+export const selectAllHouseholdMembers = (state: AppState) => {
+  return selectProfile(state).profiles.filter((x) => x.householdId === state.household.household.id);
+};
 
 export const selectCurrentProfile = (state: AppState) => {
   return selectUserProfiles(state).find((x) => x.householdId === state.household.household.id);
