548f9165600a2162984651728d5fd1b07a82abcf
Zaai90 <erik.grune@gmail.com>
2022-10-27 01:21:57 +0200

    add many piecharts

--- screens/stats/StatsScreen.tsx
+++ screens/stats/StatsScreen.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { View } from "react-native";
-import PieChartComp from "../../components/stats/PieChartComp";
+import ChorePieChart from "../../components/stats/ChorePieChart";
+import { selectChores } from "../../store/chore/choreSelectors";
+import { useAppSelector } from "../../store/store";
 
 const StatsScreen = () => {
+  const chores = useAppSelector(selectChores).chores;
+
   return (
-    <View>
-      <PieChartComp />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 0.5 }}>
+        <ChorePieChart showAvatars={true} key={"all"} />
+      </View>
+      <View style={{ flex: 0.5, backgroundColor: "red" }}>
+        {chores.map((x) => {
+          return <ChorePieChart key={x.id} size={150} showAvatars={false} choosenChore={x.id} />;
+        })}
+      </View>
     </View>
   );
 };
