c032a2cb1756dc95ff12b8da784ca1a52c4d0410
osci-the-orca <88oscste@skola.boras.se>
2022-10-27 12:09:05 +0200

    using modal to display membersettings instead redesign and stuff

--- components/profile/PendingProfiles.tsx
+++ components/profile/PendingProfiles.tsx
@@ -1,84 +1,46 @@
-// import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
-import { collection, doc, getDocs, query, updateDoc, where } from "@firebase/firestore";
-import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
 import { Surface, Text, useTheme } from "react-native-paper";
 import styled from "styled-components/native";
-import { db } from "../../config/firebase";
 import { selectHouseholdId } from "../../store/household/householdSelector";
-import { Profile } from "../../store/profile/profileModel";
-import { deleteProfile } from "../../store/profile/profileThunks";
+import { selectMemoizedCurrentProfile, selectPendingProfiles } from "../../store/profile/profileSelectors";
+import { deleteProfile, updateProfile } from "../../store/profile/profileThunks";
 import { useAppDispatch, useAppSelector } from "../../store/store";
 import AvatarCard from "../household/AvatarCard";
 
 const PendingProfiles = () => {
-  const [pendingProfiles, setPendingProfiles] = useState<Profile[]>([]);
-  const householdId = useAppSelector(selectHouseholdId);
-  const [hasChanged, setHasChanged] = useState<boolean>(false);
+  const currentProfile = useAppSelector(selectMemoizedCurrentProfile);
+  const pendingProfiles = useAppSelector(selectPendingProfiles);
   const { colors } = useTheme();
   const dispatch = useAppDispatch();
 
-  const getPendingProfiles = async (householdId: string) => {
-    try {
-      const collectionRef = collection(db, "profiles");
-      const q = query(collectionRef, where("householdId", "==", householdId), where("isApproved", "==", false));
-
-      const documentsFromQuery = await getDocs(q);
-
-      if (!documentsFromQuery.empty) {
-        const profiles: Profile[] = [];
-        documentsFromQuery.docs.forEach((doc) => profiles.push(doc.data() as Profile));
-        setPendingProfiles(profiles);
-      } else {
-        setPendingProfiles([]);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const approveProfile = async (profile: Profile) => {
-    setHasChanged(false);
-    const profilesCollection = collection(db, "profiles");
-    const q = query(profilesCollection, where("id", "==", profile.id));
-
-    const result = await getDocs(q);
-
-    if (!result.empty) {
-      const docId = result.docs[0].id;
-      await updateDoc(doc(profilesCollection, docId), { isApproved: true });
-      setHasChanged(true);
-    }
-  };
-
-  useEffect(() => {
-    getPendingProfiles(householdId);
-  }, [hasChanged]);
-
   return (
     <Container>
-      <Text>Ansökande profiler</Text>
-      {pendingProfiles.length > 0 ? (
-        <Profiles color={colors.onPrimary}>
-          {pendingProfiles.map((profile) => (
-            <ProfileCard background={colors.primaryContainer} key={profile.id}>
-              <AvatarCard profile={profile} />
-              <ButtonContainer>
-                <AcceptButton color={colors.onPrimaryContainer} onPress={async () => await approveProfile(profile)}>
-                  <MaterialIcons name="check-circle-outline" size={20} color={colors.onSecondary} />
-                </AcceptButton>
-                <RefuseButton color={colors.errorContainer} onPress={async () => dispatch(deleteProfile(profile))}>
-                  <MaterialIcons name="cancel" size={20} color={colors.onErrorContainer} />
-                </RefuseButton>
-              </ButtonContainer>
-            </ProfileCard>
-          ))}
-        </Profiles>
-      ) : (
-        <View>
-          <Text>no pending profiles</Text>
-        </View>
+      {currentProfile?.role === "owner" && pendingProfiles.length > 0 && (
+        <>
+          <Text>Ansökande profiler</Text>
+          {pendingProfiles.map(
+            (profile) =>
+              profile.isApproved === false && (
+                <ProfileCard key={profile.id}>
+                  <ProfileContent>
+                    <AvatarCard profile={profile} />
+                    <ProfileName key={profile.id} variant={"titleMedium"} style={{ marginRight: 8 }}>
+                      {profile.profileName}
+                    </ProfileName>
+                  </ProfileContent>
+                  <ButtonContainer>
+                    <IconButton background={colors.surfaceVariant} onPress={() => dispatch(updateProfile({ ...profile, isApproved: true }))}>
+                      <MaterialIcons name="check-circle-outline" size={20} color={colors.onSecondaryContainer} />
+                    </IconButton>
+                    <IconButton background={colors.errorContainer} onPress={async () => dispatch(deleteProfile(profile))}>
+                      <MaterialIcons name="cancel" size={20} color={colors.onErrorContainer} />
+                    </IconButton>
+                  </ButtonContainer>
+                </ProfileCard>
+              )
+          )}
+        </>
       )}
     </Container>
   );
@@ -87,55 +49,41 @@ const PendingProfiles = () => {
 export default PendingProfiles;
 
 const Container = styled.View`
+  margin-top: 10px;
   justify-content: center;
   align-items: center;
 `;
 
-const Profiles = styled.View<{ color: string }>`
-  background-color: ${(props) => props.color};
-  /* background-color: lightgray; */
-  width: 80%;
-`;
-
-const ProfileCard = styled(Surface)<{ background: string }>`
-  flex-direction: row;
-  padding: 8px 10px;
-  border-radius: 10px;
-  margin: 8px;
+const ProfileCard = styled(Surface)`
+  margin: 5px 10px;
   justify-content: space-between;
   align-items: center;
-  background-color: ${(props) => props.background};
+  flex-direction: row;
+  border-radius: 10px;
 `;
 
 const ProfileContent = styled.View`
+  flex: 2;
   flex-direction: row;
   align-items: center;
+  padding: 10px;
 `;
 
-const AvatarContainer = styled.View<{ avatarColor: string }>`
-  background-color: ${(props) => props.avatarColor};
-  border-radius: 4px;
-  padding: 4px;
+const ProfileName = styled(Text)`
+  margin-left: 10px;
 `;
 
 const ButtonContainer = styled.View`
+  flex: 1;
   flex-direction: row;
 `;
 
-const AcceptButton = styled.Pressable<{ color: string }>`
-  background-color: ${(props) => props.color};
-  align-items: center;
+const IconButton = styled.Pressable<{ background: string }>`
+  flex: 1;
+  background-color: ${(props) => props.background};
   justify-content: center;
-  border-radius: 1000px;
-  margin: 0 10px;
-  padding: 2px;
-`;
-
-const RefuseButton = styled.Pressable<{ color: string }>`
-  background-color: ${(props) => props.color};
-  border-radius: 1000px;
   align-items: center;
-  justify-content: center;
-  margin: 0 10px;
-  padding: 2px;
+  border-radius: 4px;
+  padding: 10px 0px;
+  margin-right: 10px;
 `;
