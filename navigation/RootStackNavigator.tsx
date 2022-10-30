994a51034615712e924fa36ce6011994462da157
osci-the-orca <88oscste@skola.boras.se>
2022-10-30 10:21:04 +0100

    render different stacks depending if logged in or not

--- navigation/RootStackNavigator.tsx
+++ navigation/RootStackNavigator.tsx
@@ -4,6 +4,8 @@ import HouseholdOptionsScreen from "../screens/HouseholdOptionsScreen";
 import CreateUserScreen from "../screens/user/CreateUserScreen";
 import LoginScreen from "../screens/user/LoginScreen";
 import WelcomeScreen from "../screens/user/WelcomeScreen";
+import { useAppSelector } from "../store/store";
+import { selectUser } from "../store/user/userSelectors";
 import TabStackNavigator from "./TabStackNavigator";
 
 export type RootStackParams = {
@@ -17,13 +19,22 @@ export type RootStackParams = {
 const RootStack = createNativeStackNavigator<RootStackParams>();
 
 const RootNavigator = () => {
+  const user = useAppSelector(selectUser);
   return (
     <RootStack.Navigator>
-      <RootStack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
-      <RootStack.Screen name="Login" component={LoginScreen} options={{ headerTitle: "Logga in", headerBackVisible: false }} />
-      <RootStack.Screen name="TabStack" component={TabStackNavigator} options={{ headerShown: false }} />
-      <RootStack.Screen name="CreateUser" component={CreateUserScreen} options={{ headerTitle: "Registrera konto", headerBackVisible: false }} />
-      <RootStack.Screen name="HouseholdOptions" component={HouseholdOptionsScreen} options={{ headerShown: false, gestureEnabled: false }} />
+      {!user && (
+        <>
+          <RootStack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+          <RootStack.Screen name="Login" component={LoginScreen} options={{ headerTitle: "Logga in", headerBackVisible: false }} />
+          <RootStack.Screen name="CreateUser" component={CreateUserScreen} options={{ headerTitle: "Registrera konto", headerBackVisible: false }} />
+        </>
+      )}
+      {user && (
+        <>
+          <RootStack.Screen name="TabStack" component={TabStackNavigator} options={{ headerShown: false }} />
+          <RootStack.Screen name="HouseholdOptions" component={HouseholdOptionsScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        </>
+      )}
     </RootStack.Navigator>
   );
 };
