fa33457d63795223a789b81d680eea94866549ea
osci-the-orca <88oscste@skola.boras.se>
2022-11-02 08:23:38 +0100

    pending profile will now update when someone apply to join household

--- components/profile/PendingProfiles.tsx
+++ components/profile/PendingProfiles.tsx
@@ -1,18 +1,33 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import React from "react";
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
+import React, { useEffect } from "react";
 import { View } from "react-native";
 import { Divider, Surface, Text, useTheme } from "react-native-paper";
 import styled from "styled-components/native";
+import { db } from "../../config/firebase";
 import { selectCurrentProfile, selectPendingProfiles } from "../../store/profile/profileSelectors";
-import { deleteProfile, updateProfile } from "../../store/profile/profileThunks";
+import { deleteProfile, getAllProfiles, updateProfile } from "../../store/profile/profileThunks";
 import { useAppDispatch, useAppSelector } from "../../store/store";
+import { selectUser } from "../../store/user/userSelectors";
 import AvatarCard from "../household/AvatarCard";
 
 const PendingProfiles = () => {
   const currentProfile = useAppSelector(selectCurrentProfile);
   const pendingProfiles = useAppSelector(selectPendingProfiles);
+  const user = useAppSelector(selectUser);
   const { colors } = useTheme();
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const profilesCollection = collection(db, "profiles");
+    const q = query(profilesCollection, where("isApproved", "==", false), where("householdId", "==", currentProfile?.householdId));
+
+    onSnapshot(q, (snapshot) => {
+      if (user) {
+        dispatch(getAllProfiles(user));
+      }
+    });
+  }, []);
 
   return (
     <View>
