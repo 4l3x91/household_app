a7c6e0b21c8a1e8e5457848fe58ccdf4551d8f62
osci-the-orca <88oscste@skola.boras.se>
2022-11-02 16:02:34 +0100

    added listerns for chores and completedChores

--- hooks/useFirestoreListeners.ts
+++ hooks/useFirestoreListeners.ts
@@ -1,8 +1,10 @@
 import { collection, onSnapshot, query, where } from "@firebase/firestore";
 import { useEffect } from "react";
 import { db } from "../config/firebase";
+import { getChores } from "../store/chore/choreThunks";
+import { getCompletedChoresPerHousehold } from "../store/completedChore/completedChoreThunks";
 import { selectHousehold } from "../store/household/householdSelector";
-import { selectMemoizedCurrentProfile } from "../store/profile/profileSelectors";
+import { selectAllHouseholdMembers, selectMemoizedCurrentProfile } from "../store/profile/profileSelectors";
 import { getAllProfiles } from "../store/profile/profileThunks";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { selectUser } from "../store/user/userSelectors";
@@ -11,24 +13,52 @@ export const useFirestoreListeners = () => {
   const user = useAppSelector(selectUser);
   const currentProfile = useAppSelector(selectMemoizedCurrentProfile);
   const household = useAppSelector(selectHousehold);
+  const members = useAppSelector(selectAllHouseholdMembers);
   const dispatch = useAppDispatch();
-  console.log("useFirestoreListeners");
-  console.log(currentProfile?.profileName);
 
   useEffect(() => {
     if (currentProfile && household) {
-      console.log("useEffect running");
       const profilesCollection = collection(db, "profiles");
       const q = query(profilesCollection, where("isApproved", "==", false), where("householdId", "==", currentProfile?.householdId));
 
-      const unsub = onSnapshot(q, (snapshot) => {
+      const unsub = onSnapshot(q, () => {
         if (user) {
+          console.log("changes in profiles");
           dispatch(getAllProfiles(user));
-          console.log("in snapshot");
         }
       });
 
       return () => unsub();
+    }
+  }, [household]);
+
+  useEffect(() => {
+    if (household) {
+      const choresCollection = collection(db, "chores");
+      const q = query(choresCollection, where("householdId", "==", household.household.id));
+
+      const unsubscribe = onSnapshot(q, () => {
+        console.log("changes in chores");
+        dispatch(getChores(household.household.id));
+      });
+
+      return () => unsubscribe();
+    }
+  }, [household]);
+
+  useEffect(() => {
+    if (household.household && members.length !== 0) {
+      const completedChoresCollection = collection(db, "completedChores");
+      const membersIds = members.map((x) => x.id);
+
+      const q = query(completedChoresCollection, where("profileId", "in", membersIds));
+
+      const unsubscribe = onSnapshot(q, () => {
+        console.log("changes in completed chores");
+        dispatch(getCompletedChoresPerHousehold(members));
+      });
+
+      return () => unsubscribe();
     }
   }, [household]);
 };
